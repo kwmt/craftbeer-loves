93390f24667d225519b2033bdf240addd5d1d98f
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-10 19:36:00 +0900

    recreate project

--- app/README.md
+++ app/README.md
@@ -1,6 +1,6 @@
 # app
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.craftbeerloves.app
+package com.example.app
 
 import io.flutter.embedding.android.FlutterActivity
 
